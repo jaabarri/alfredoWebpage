b4f3e4e98ab1b1422903a4ffa51ec41054306c65
jaabarri <jaabarri1112@gmail.com>
2025-02-28 14:59:24 -0800

    Header buttons and outlining done

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src="./assets/img.png"></img>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Services</a></li>
+                <li><a href='#'>Gallery</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+        </header>
+    )
+}
