965309e73dc0b44580d5f8329818dc33501b8a16
jaabarri <jaabarri1112@gmail.com>
2025-03-07 15:06:21 -0800

    video element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 export default function Hero() {
     return (
         <section>
-            
+            <video autoPlay muted loop className="event-Loop">
+            <source src="events.mp4" type="video/mp4">
+            </video>
         </section>
     )
 }
